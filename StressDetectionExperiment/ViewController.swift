cd2e6c38df601ec1de0c2cd5c32959de7fcb1ad0
Fabio Miyagawa <fabiomiyagawa@client-10-229-124-244.tamulink.tamu.edu>
2020-01-28 14:55:28 -0600

    Initial Commit

---
+++ StressDetectionExperiment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  StressDetectionExperiment
+//
+//  Created by Fabio Miyagawa on 1/28/20.
+//  Copyright © 2020 TAMU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
